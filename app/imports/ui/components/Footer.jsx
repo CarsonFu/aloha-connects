179a014cc43ae5106f6213bd09345e9717c2fc7b
chanj200 <chanj200@hawaii.edu>
2024-07-11 13:24:02 -1000

    coloring nav bar and footer

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -3,9 +3,9 @@ import { Col, Container } from 'react-bootstrap';
 
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 const Footer = () => (
-  <footer className="mt-auto py-3 bg-light">
-    <Container>
-      <Col className="text-center">
+  <footer className="footer mt-auto py-3">
+    <Container className ="">
+      <Col className="footer-content">
         Add Info
         {' '}
         <br />
--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -13,7 +13,7 @@ const NavBar = () => {
   }), []);
 
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar className="navbar" expand="lg">
       <Container>
         <Navbar.Brand as={NavLink} to="/">
           <h2>ALOHA Connects</h2>
